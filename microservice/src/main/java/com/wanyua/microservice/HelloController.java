cefc75396dd567585def0264b0f26fe1794154a1
samwanyua <samexample8@gmail.com>
2024-05-27 14:09:44 +0300

    Create springboot mvc project

--- microservice/src/main/java/com/wanyua/microservice/HelloController.java
+++ microservice/src/main/java/com/wanyua/microservice/HelloController.java
@@ -1,6 +1,8 @@
 package com.wanyua.microservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // tells spring boot that the call is a controller and can handle http requests
@@ -8,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello") // endpoints are defined in this file
     public String hello(){
         return  "SpringBoot Microservices";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello" + name + "!";
     }
 }
